fcf290077e15988b24a370de0b56a79c767f1a3e
lesia-s <lesiiasergienko@gmail.com>
2020-04-01 16:24:08 +0300

    Travis fix

--- .install-bood_rebase.sh
+++ .install-bood_rebase.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -ex
-wget https://github.com/downloads/tnsts/design-practice-2/examples/binary/out/bin/bood_rebase
+wget https://github.com/tnsts/design-practice-2/raw/master/examples/binary/out/bin/bood_rebase
 sudo chmod +x bood_rebase
 mv bood_rebase $GOPATH/bin
