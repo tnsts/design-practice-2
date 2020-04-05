0dd0a92719c355efb3384359be1aef4a64e79262
lesia-s <lesiiasergienko@gmail.com>
2020-04-05 03:47:34 +0300

    Travis

--- .install-bood_rebase.sh
+++ .install-bood_rebase.sh
@@ -1,4 +1,3 @@
 wget https://github.com/roman-mazur/bood/releases/download/v0.0.5/bood-linux
-sudo mv bood-linux bood_rebase
-sudo chmod +x bood_rebase
-mv bood_rebase $GOPATH/bin
+sudo chmod +x bood-linux
+mv bood-linux $GOPATH/bin
