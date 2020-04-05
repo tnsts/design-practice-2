ad3557659c40a921de780ad68bfa5362fa66695a
lesia-s <lesiiasergienko@gmail.com>
2020-04-05 03:44:01 +0300

    Travis

--- .install-bood_rebase.sh
+++ .install-bood_rebase.sh
@@ -1,3 +1,4 @@
-wget https://github.com/tnsts/design-practice-2/raw/master/examples/binary/out/bin/bood_rebase
+wget https://github.com/roman-mazur/bood/releases/download/v0.0.5/bood-linux
+sudo mv bood-linux bood_rebase
 sudo chmod +x bood_rebase
 mv bood_rebase $GOPATH/bin
